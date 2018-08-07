b9c6d8e398ea0fc6ae89662e4d1a1f9395a565af
Adnan Hafeez <adnanhr81@gmail.com>
2018-08-08 07:48:23 +1000

    Added a python server code. Changed the tensorflow chatbot code to return values instead of printing. Added a little more debugging print statements

--- contextual_chatbot/pythonServer.py
+++ contextual_chatbot/pythonServer.py
@@ -1,21 +1,21 @@
 from flask import (
     Flask,
-    render_template
 )
 
+#import the code for chatbot
+from tensorflow_chatbot import *
+
 # Create the application instance
-app = Flask(__name__, template_folder="templates")
-
-# Create a URL route in our application for "/"
-@app.route('/')
-def home():
-    """
-    This function just responds to the browser ULR
-    localhost:5000/
-
-    :return:        the rendered template 'home.html'
-    """
-    return render_template('./home.html')
+app = Flask(__name__)
+# Create a URL route in our application for "query"
+@app.route('/query/<question>')
+def query(question):
+    #Get a response from chatbot
+    print("Question received = " + question)
+    res = response(question)
+    #Printing the repsone to console just for debugging purposes
+    print("Response generated = " + res)
+    return res
 
 # If we're running in stand alone mode, run the application
 if __name__ == '__main__':
---
+++ contextual_chatbot/tensorflow_chatbot.py
@@ -0,0 +1,131 @@
+# things we need for NLP
+import nltk
+from nltk.stem.lancaster import LancasterStemmer
+stemmer = LancasterStemmer()
+
+import warnings
+with warnings.catch_warnings():
+    warnings.filterwarnings("ignore",category=FutureWarning)
+    import h5py
+
+
+# things we need for Tensorflow
+import numpy as np
+import tflearn
+import tensorflow as tf
+import random
+
+# restore all of our data structures
+import pickle
+data = pickle.load( open( "training_data", "rb" ) )
+words = data['words']
+classes = data['classes']
+train_x = data['train_x']
+train_y = data['train_y']
+
+tf.logging.set_verbosity(tf.logging.ERROR)
+
+# import our chat-bot intents file
+import json
+with open('faq_intents_updated.json') as json_data:
+    intents = json.load(json_data)
+
+
+# Build neural network
+net = tflearn.input_data(shape=[None, len(train_x[0])])
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net, 32)
+net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+net = tflearn.regression(net)
+
+# Define model and setup tensorboard
+model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+def clean_up_sentence(sentence):
+    # tokenize the pattern
+    sentence_words = nltk.word_tokenize(sentence)
+    # stem each word
+    sentence_words = [stemmer.stem(word.lower()) for word in sentence_words]
+    return sentence_words
+
+# return bag of words array: 0 or 1 for each word in the bag that exists in the sentence
+def bow(sentence, words, show_details=False):
+    # tokenize the pattern
+    sentence_words = clean_up_sentence(sentence)
+    # bag of words
+    bag = [0]*len(words)
+    for s in sentence_words:
+        for i,w in enumerate(words):
+            if w == s:
+                bag[i] = 1
+                if show_details:
+                    print ("found in bag: %s" % w)
+
+    return(np.array(bag))
+
+# load our saved model
+model.load('./model.tflearn')
+
+# create a data structure to hold user context
+context = {}
+
+ERROR_THRESHOLD = 0.25
+def classify(sentence):
+    # generate probabilities from the model
+    results = model.predict([bow(sentence, words)])[0]
+    # filter out predictions below a threshold
+    results = [[i,r] for i,r in enumerate(results) if r>ERROR_THRESHOLD]
+    # sort by strength of probability
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = []
+    for r in results:
+        return_list.append((classes[r[0]], r[1]))
+    # return tuple of intent and probability
+    return return_list
+
+def response(sentence, userID='123', show_details=False):
+    results = classify(sentence)
+    # if we have a classification then find the matching intent tag
+    if results:
+        # loop as long as there are matches to process
+        while results:
+            for i in intents['intents']:
+                # find a tag matching the first result
+                if i['tag'] == results[0][0]:
+                    # set context for this intent if necessary
+                    if 'context_set' in i:
+                        if show_details: print ('context:', i['context_set'])
+                        context[userID] = i['context_set']
+
+                    # check if this intent is contextual and applies to this user's conversation
+                    if not 'context_filter' in i or \
+                        (userID in context and 'context_filter' in i and i['context_filter'] == context[userID]):
+                        if show_details: print ('tag:', i['tag'])
+                        # a random response from the intent
+                        return (random.choice(i['responses']))
+
+            results.pop(0)
+    #Added a little expception handling to deal with empty message
+    else:
+        try:
+            #Removed the print statement
+            ans = random.choice(negative_answers) + '\n' + repharse
+            print("Answer generated in chatbot = " + ans)
+            return (random.choice(negative_answers) + '\n' + rephrase)
+        except:
+            return("Exception raised in response")
+
+
+
+negative_answers = ["Sorry I didn’t get that!", "Sorry, I don't understand what you are trying to say.", "I have not learned how to answers that question yet"]
+rephrase = "Please try rephrasing your question."
+
+#
+#def generate_response():
+#    user_input = input()
+#    print ('')
+#    response(user_input)
+#    print ('')
+#    generate_response()
+
+#generate_response()
