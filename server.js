b9ae0e78012f7735835da5241fe1e14321cebf69
Nguyen Minh Nhat Pham <phamngu@deakin.edu.au>
2018-08-13 19:59:12 +1000

    Add a simple demo from IBM Watson Assistant

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
-const express = require('express')
-const app = express()
-app.use(express.static('public'))
-app.listen(process.env.PORT || 8080, () => console.log("Running Good!"))
+const express = require('express');
+const app = express();
+
+app.use('/watson-assistant', express.static('WatsonAssistantDemo'));
+
+app.use(express.static('public'));
+app.listen(process.env.PORT || 8080, () => console.log("Running Good!"));
