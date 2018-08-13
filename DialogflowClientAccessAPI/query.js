e763c9927a54a0aba05d5cd1e7bec4367f1c70da
Adnan Hafeez <adnanhr81@gmail.com>
2018-08-14 08:38:23 +1000

    Added function calls to Dialogflow. Problems with asynching calls to Dialogflow API

--- DialogflowClientAccessAPI/query.js
+++ DialogflowClientAccessAPI/query.js
@@ -1,53 +1,57 @@
-// You can find your project ID in your Dialogflow agent settings
-const projectId = 'myfirstagent-73433';
-const sessionId = 'quickstart-session-id';
-
-// Instantiate a DialogFlow client.
 const dialogflow = require('dialogflow');
-const sessionClient = new dialogflow.SessionsClient();
+//Flag for printing debugging information
+const LOGYES = true;
 
-
+//Custom defined Dialogflow class for encapsulating access to DialogFlow API
 class DialogFlow {
   constructor (projectId,sessionId) {
     //Set the project and session Ids
     this.projectId = projectId;
     this.sessionId = sessionId;
     this.languageCode = 'en-US';
-
     //Get a new DialogflowClient
     this.sessionClient = new dialogflow.SessionsClient();
     // Define session path
-    this.sessionPath = sessionClient.sessionPath(this.projectId, this.sessionId);
+    this.sessionPath = this.sessionClient.sessionPath(this.projectId, this.sessionId);
   }
 
   async GetReplyFromDialogflow(question){
-
     // The text query request.
     let request = {
-      session: sessionPath,
+      session: this.sessionPath,
       queryInput: {
         text: {
           text: question,
-          languageCode: languageCode,
+          languageCode: this.languageCode,
         },
       },
     };
     // Send request and log result
-    sessionClient
+    this.sessionClient
       .detectIntent(request)
       .then(responses => {
-        console.log('Detected intent');
         const result = responses[0].queryResult;
-        console.log(`  Query: ${result.queryText}`);
-        console.log(`  Response: ${result.fulfillmentText}`);
-        if (result.intent) {
-          console.log(`  Intent: ${result.intent.displayName}`);
-        } else {
-          console.log(`  No intent matched.`);
-        }
+        if(LOGYES) {
+         console.log('Detected intent');
+         console.log(`  Query: ${result.queryText}`);
+         console.log(`  Response: ${result.fulfillmentText}`);
+         return result.fulfillmentText;
+      }
+
       })
       .catch(err => {
-        console.error('ERROR:', err);
+        if (LOGYES) console.error('ERROR:', err);
+        throw err;
       });
   }
+}
+
+let DialogFlowBot = new DialogFlow('myfirstagent-73433','quickstart-session-id');
+
+try {
+  let res =  DialogFlowBot.GetReplyFromDialogflow('Good evening?');
+  console.log("Response = " + res);
+}
+catch(e) {
+  console.error('ERROR:', e)
 }
