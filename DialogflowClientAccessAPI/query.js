5518acd2bad50be51fca3ce1afb5771aaf26daeb
Adnan Hafeez <adnanhr81@gmail.com>
2018-08-14 08:11:17 +1000

    Added a Dialogflow class in query.js

--- DialogflowClientAccessAPI/query.js
+++ DialogflowClientAccessAPI/query.js
@@ -1,8 +1,6 @@
 // You can find your project ID in your Dialogflow agent settings
 const projectId = 'myfirstagent-73433';
 const sessionId = 'quickstart-session-id';
-const query = 'What is your interest rate?';
-const languageCode = 'en-US';
 
 // Instantiate a DialogFlow client.
 const dialogflow = require('dialogflow');
@@ -10,7 +8,6 @@ const sessionClient = new dialogflow.SessionsClient();
 
 
 class DialogFlow {
-
   constructor (projectId,sessionId) {
     //Set the project and session Ids
     this.projectId = projectId;
@@ -24,44 +21,33 @@ class DialogFlow {
   }
 
   async GetReplyFromDialogflow(question){
-    
 
-
+    // The text query request.
+    let request = {
+      session: sessionPath,
+      queryInput: {
+        text: {
+          text: question,
+          languageCode: languageCode,
+        },
+      },
+    };
+    // Send request and log result
+    sessionClient
+      .detectIntent(request)
+      .then(responses => {
+        console.log('Detected intent');
+        const result = responses[0].queryResult;
+        console.log(`  Query: ${result.queryText}`);
+        console.log(`  Response: ${result.fulfillmentText}`);
+        if (result.intent) {
+          console.log(`  Intent: ${result.intent.displayName}`);
+        } else {
+          console.log(`  No intent matched.`);
+        }
+      })
+      .catch(err => {
+        console.error('ERROR:', err);
+      });
   }
-
-
 }
-
-
-
-// Define session path
-const sessionPath = sessionClient.sessionPath(projectId, sessionId);
-
-// The text query request.
-const request = {
-  session: sessionPath,
-  queryInput: {
-    text: {
-      text: query,
-      languageCode: languageCode,
-    },
-  },
-};
-
-// Send request and log result
-sessionClient
-  .detectIntent(request)
-  .then(responses => {
-    console.log('Detected intent');
-    const result = responses[0].queryResult;
-    console.log(`  Query: ${result.queryText}`);
-    console.log(`  Response: ${result.fulfillmentText}`);
-    if (result.intent) {
-      console.log(`  Intent: ${result.intent.displayName}`);
-    } else {
-      console.log(`  No intent matched.`);
-    }
-  })
-  .catch(err => {
-    console.error('ERROR:', err);
-  });
