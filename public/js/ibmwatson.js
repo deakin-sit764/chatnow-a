0cec48c43fe22eb569d2e3c9753e168a369c3263
Nguyen Minh Nhat Pham <phamngu@deakin.edu.au>
2018-08-15 19:43:25 +1000

    Update Watson button to show the chatbot

--- public/js/ibmwatson.js
+++ public/js/ibmwatson.js
@@ -1,3 +1,4 @@
 // Function to start the IBM Watson chatbot widget
-    function startWatsonChat() {
-      }
+function startWatsonChat() {
+    window.open("./watson-assistant", "Chatnow-a", "status=0, width=400, height=500");
+};
