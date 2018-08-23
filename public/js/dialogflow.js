356aac3dce72e04992d1aff30d773dbf54d34af8
NATHAN HUGHES <nhug@deakin.edu.au>
2018-08-23 22:09:17 +1000

    Dialogflow API call test

--- public/js/dialogflow.js
+++ public/js/dialogflow.js
@@ -4,3 +4,21 @@
     	var botstring = "\<iframe allow=\"microphone;\"width=\"380\" height=\"480\" src=\"https://console.dialogflow.com/api-client/demo/embedded/31a55a15-f92f-4e86-b712-b92070d8028b\"></iframe>";
     	myWindow.document.write(botstring);
     }
+
+function dialogapi() {
+    var http = new XMLHttpRequest();
+    var url = '/Dialogflow/query';
+    var params = 'question=hello';
+    http.open('POST', url, true);
+
+    //Send the proper header information along with the request
+    http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+
+    http.onreadystatechange = function() {//Call a function when the state changes.
+      if(http.readyState == 4 && http.status == 200) {
+          console.log(http.responseText);
+        }
+      }
+      http.send(params);
+      console.log(http);
+    }
